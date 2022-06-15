e8f623c637d920d8153eb5ed135fc85cbfac1a07
saidugurlu <ugurlumsaid@gmx.de>
2022-06-15 13:18:20 +0200

    edit return

--- views.js
+++ views.js
@@ -1,10 +1,20 @@
-export const siteView = ({nouns}) => {
-   return `
-    <h2>There are ${nouns.length} nouns</h2>
-    <ul>${nouns
-      .map((m) => `<li>${m.article} ${m.singular}</li>`)
-      .join("")}</ul>`
-       /*  mapten bir array gelir ve array de otomatik olarak virgül konulur. 
-       Bunu kaldirmak icin join() metodunu kullaniriz. */
+export const siteView = ({ nouns }) => {
+  return `<!DOCTYPE html>
+   <html lang="en">
+   <head>
+       <meta charset="UTF-8">
+       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title>Node-Express</title>
+   </head>
+   <body>
+   <h2>There are ${nouns.length} nouns</h2>
+   <ul>${nouns.map((m) => `<li>${m.article} ${m.singular}</li>`).join("")}</ul>
+     /*  mapten bir array gelir ve array de otomatik olarak virgül konulur. 
+     Bunu kaldirmak icin join() metodunu kullaniriz. */
 
-}
+   </body>
+   </html>
+   
+   `;
+};
