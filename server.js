641620d7454fef9ba57f711b2a935db7d4c23879
saidugurlu <ugurlumsaid@gmx.de>
2022-06-15 11:25:23 +0200

    schowing nouns

--- server.js
+++ server.js
@@ -7,14 +7,17 @@ const port = 3009;
 const nouns = (
   await axios.get("https://edwardtanguay.netlify.app/share/germanNouns.json")
 ).data;
- 
 
-app.get('/', (req, res) => {
-    res.send(
-        `<ul>${nouns
-            .map((m) => `<li>${m.article} ${m.singular}</li>`)
-            .join('')}</ul>`
-    );
+app.get("/", (req, res) => {
+  res.send(
+    `
+        <h1>There are ${nouns.length} nouns</h1>
+        <ul>${nouns
+          .map((m) => `<li>${m.article} ${m.singular}</li>`)
+          .join("")}</ul>`
+           /*  mapten bir array gelir ve array de otomatik olarak virgül konulur. 
+           Bunu kaldirmak icin join() metodunu kullaniriz. */
+  );
 });
 
 app.listen(port, () => {
