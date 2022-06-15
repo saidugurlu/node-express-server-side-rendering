07f32ed53cb810055daed868e3c50d99825b9e47
saidugurlu <ugurlumsaid@gmx.de>
2022-06-15 11:10:28 +0200

    data displaed

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-import express from 'express';
+import axios from "axios";
+import express from "express";
 
 const app = express();
 const port = 3009;
 
+const nouns = (
+  await axios.get("https://edwardtanguay.netlify.app/share/germanNouns.json")
+).data;
+ 
+
 app.get('/', (req, res) => {
-    res.send('welcome to this <b>website</b>');
+    res.send(
+        `<ul>${nouns
+            .map((m) => `<li>${m.article} ${m.singular}</li>`)
+            .join('')}</ul>`
+    );
 });
 
 app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`);
+  console.log(`Listening at http://localhost:${port}`);
 });
